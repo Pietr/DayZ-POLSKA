36408d512ba6f35cb5ec55871761c6cc5805f597
Pietr <snajperkrs@gmail.com>
2017-04-06 14:42:56 +0100

    Dodano serial

--- DayZPL_DAYZ/inventory.lua
+++ DayZPL_DAYZ/inventory.lua
@@ -2841,7 +2841,7 @@ end
 --Тест команди для проверки функционала
 addCommandHandler("items",function()
 	local serial = getPlayerSerial(getLocalPlayer())
-	if serial == "A80157878519CE6B328D3CA9B7609B12" then
+	if serial == "A80157878519CE6B328D3CA9B7609B12" or serial == "1FFF705136AF698B22888A6BF243C971" then
 		for id, item in ipairs(inventoryItems["Weapons"]["Primary Weapon"]) do
 			setElementData(getLocalPlayer(),item[1],1)
 		end
@@ -2880,7 +2880,7 @@ addCommandHandler("items",function()
 end)
 addCommandHandler("itemsd",function()
 	local serial = getPlayerSerial(getLocalPlayer())
-	if serial == "A80157878519CE6B328D3CA9B7609B12" then
+	if serial == "A80157878519CE6B328D3CA9B7609B12" or serial == "1FFF705136AF698B22888A6BF243C971" then
 		for id, item in ipairs(inventoryItems["Weapons"]["Primary Weapon"]) do
 			setElementData(getLocalPlayer(),item[1],0)
 		end
