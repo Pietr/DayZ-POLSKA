e74b77312d0947b62e7374e27dfb3f979ff5aae0
Pietr <snajperkrs@gmail.com>
2017-04-22 19:17:49 +0200

    Kamera admina znów dostępna tylko dla administracji

--- DayZPL_camhack/admincam.lua
+++ DayZPL_camhack/admincam.lua
@@ -1,17 +1,17 @@
 ﻿function toggleAdminCam(player)
---if isObjectInACLGroup("user."..getAccountName(getPlayerAccount(player)),aclGetGroup("Admin")) then 
-if not isPedInVehicle(player) then
-	if isPlayerFreecamEnabled(player) then
-		setPlayerFreecamDisabled(player)
-		outputChatBox("Wyłączyłeś tryb samolotowy", player, 255, 0, 0) 
-	else 
-		setPlayerFreecamEnabled(player)
-		outputChatBox("Włączyłeś tryb samolotowy", player, 255, 0, 255) 
+	if isObjectInACLGroup("user."..getAccountName(getPlayerAccount(player)),aclGetGroup("Admin")) then 
+		if not isPedInVehicle(player) then
+			if isPlayerFreecamEnabled(player) then
+				setPlayerFreecamDisabled(player)
+				outputChatBox("Wyłączyłeś tryb samolotowy", player, 255, 0, 0) 
+			else 
+				setPlayerFreecamEnabled(player)
+				outputChatBox("Włączyłeś tryb samolotowy", player, 255, 0, 255) 
+			end 
+		else 
+			outputChatBox("Nie możesz tego używać w samochodzie!", player, 255, 0, 0) 
+		end 
 	end 
-else 
-	outputChatBox("Nie możesz tego używać w samochodzie!", player, 255, 0, 0) 
-end 
---end 
 end
 addCommandHandler("admincam",toggleAdminCam)
 
